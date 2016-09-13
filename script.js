c1630bb0b20813aabcc9ee1e954d87b870f78a2b
egpavelka <elwynpavelka@gmail.com>
2016-09-13 04:21:59 -0400

    never too early to commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// GET GEOLOCATION
+function getLocation() {
+  if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(
+  // success callback
+    function(position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    // create current conditions URL
+    weatherURL = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long;
+    // create forecast URL
+    forecastURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long;
+  },
+// error callback
+alert("Your location cannot be detected.  Please enter a valid U.S. postal code to continue.")
+);
+  }
+  else {
+    // Prompt for zip
+  }
+
+}
+
+// GET ZIPCODE
+function getZip() {
+
+}
+
+// FETCH CURRENT CONDITIONS URL
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
+// api.openweathermap.org/data/2.5/weather?zip=94040,us
+
+// FETCH FORECAST URL
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
+
+// RETURN JSON
+$.getJSON(weatherURL, function(data) {
+
+});
+
+$.getJSON(forecastURL, function(data) {
+
+});
+
+/*
+CONDITIONS:
+clear sky
+few clouds
+scattered clouds
+broken clouds
+shower rain
+rain
+thunderstorm
+snow
+mist*
+fog*
+*/
