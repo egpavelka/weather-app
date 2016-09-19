19ad0fe1d0dcd3e81849f1c9cecf46898a321e1c
egpavelka <elwynpavelka@gmail.com>
2016-09-18 23:28:22 -0400

    added calls for hourly and daily forecast data

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 // GET GEOLOCATION
 function getLocation() {
-  if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(
-  // success callback
-    function(position) {
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-    // create current conditions URL
-    weatherURL = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=831f9a0e76c47eb878b49f28785cd20b";
-    // create forecast URL
-    forecastURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&APPID=831f9a0e76c47eb878b49f28785cd20b";
-  },
-// error callback
-alert("Your location cannot be detected.  Please enter a valid U.S. postal code to continue.")
-);
-  }
-  else {
-    // Prompt for zip
-  }
-
+//   if ("geolocation" in navigator) {
+//   navigator.geolocation.getCurrentPosition(
+//   // success callback
+//     function(position) {
+//     lat = position.coords.latitude;
+//     long = position.coords.longitude;
+//     // create current conditions URL
+//     weatherURL = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=831f9a0e76c47eb878b49f28785cd20b";
+//     // create forecast URL
+//     forecastURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&APPID=831f9a0e76c47eb878b49f28785cd20b";
+//   },
+// // error callback
+// alert("Your location cannot be detected.  Please enter a valid U.S. postal code to continue.")
+// );
+//   }
+//   else {
+//     // Prompt for zip
+//   }
+// temp
+weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b";
+hourlyURL = "http://api.openweathermap.org/data/2.5/forecast?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b";
+  // Send URLs to getJSON
+  getWeather(weatherURL, forecastURL);
 }
 
 // GET ZIPCODE
@@ -30,18 +34,53 @@ function getZip() {
 // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
 // api.openweathermap.org/data/2.5/weather?zip=94040,us
 
-// FETCH FORECAST URL
+// FETCH HOURLY FORECAST URL
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
 
+/*
+### Day/Night:
+- Current time
+- Local sunrise/sunset
+
+### Current weather conditions:
+- Temp
+weatherData.main.temp
+- Conditions
+weatherData.weather.id (code)
+weatherData.weather.main (e.g. clouds)
+weatherData.weather.description (e.g. overcast clouds)
+weatherData.weather.icon (icon)
+
+- Current day
+
+- Today's high/low
+
+
+### Hourly forecast:
+- Current time
+- Forecast times
+- Forecast temps
+- Forecast conditions
+
+### Day forecast:
+- Current day
+- Forecast days
+- Forecast highs
+- Forecast lows
+- Forecast conditions
+*/
+
 // RETURN JSON
-$.getJSON(weatherURL, function(data) {
-
+function getWeather(weatherURL /*, forecastURL*/) {
+  // Current conditions
+$.getJSON(weatherURL, function(weatherData) {
+  console.log(weatherData.main.temp);
 });
 
-$.getJSON(forecastURL, function(data) {
-
-});
-
+// $.getJSON(hourlyURL, function(hourlyData) {
+//
+// });
+}
 /*
 CONDITIONS:
 clear sky
@@ -55,3 +94,5 @@ snow
 mist*
 fog*
 */
+// RUN IT
+getLocation();
