77b1090521198ef5f62c9fe93922fcd5731a8219
egpavelka <elwynpavelka@gmail.com>
2016-09-23 06:40:53 -0400

    more angular

---
+++ app.js
@@ -0,0 +1,17 @@
+function() {
+// APP
+var app = angular.module('weatherApp', []);
+
+app.controller('CurrentController', function() {
+  this.data = currentData;
+});
+
+app.controller('CurrentController', function() {
+  this.data = currentData;
+});
+
+app.controller('CurrentController', function() {
+  this.data = currentData;
+});
+
+}
