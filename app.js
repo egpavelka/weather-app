fd57d5e170ba27a9ede9024430168216e6c51414
egpavelka <elwynpavelka@gmail.com>
2016-10-18 04:25:56 -0400

    weather data fetches and displays from static JSON URLs

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 
-function() {
-  // APP
+(function() {
+  'use strict';
+
+// APP
 var app = angular.module('weatherApp', []);
 
-app.controller('weatherController', [ '$http', '$scope', function($scope, $http) {
-    //current
-    $http.get('http://api.openweathermap.org/data/2.5/weather?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
-    .success(function(response){
-      $scope.current = response.data;
-      console.log(current);
-    });
+app.controller('weatherController', [ '$http', function($http) {
+  var weather = this;
+  weather.current = [];
+  weather.hourly = [];
+  weather.daily = [];
+
+  //current
+$http.get('http://api.openweathermap.org/data/2.5/weather?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
+.success(function(response) {
+  weather.current = response;
+})
+// hourly
+.then(
+$http.get('http://api.openweathermap.org/data/2.5/forecast?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
+.success(function(response) {
+  weather.hourly = response;
+  console.log(response);
+
+}))
+// daily
+.then(
+$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
+.success(function(response) {
+  weather.daily = response;
+    console.log(response);
+}));
+
 
 
 }]);
 
-}
+})();
