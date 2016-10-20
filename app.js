afe74553e529f7a13a77dadea9066a377c3c063e
egpavelka <elwynpavelka@gmail.com>
2016-10-20 02:17:42 -0400

    good shape before moving to geolocation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = angular.module('weatherApp', []);
 
 app.controller('weatherController', [ '$http', function($http) {
   var weather = this;
-  
+
   weather.current = [];
   weather.hourly = [];
   weather.daily = [];
@@ -21,7 +21,6 @@ app.controller('weatherController', [ '$http', function($http) {
   $http.get('http://api.openweathermap.org/data/2.5/forecast?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
   .success(function(response) {
     weather.hourly = response;
-    console.log(response);
 
   }))
   // daily
@@ -29,9 +28,7 @@ app.controller('weatherController', [ '$http', function($http) {
   $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
   .success(function(response) {
     weather.daily = response;
-      console.log(response);
   }));
-
 // Change the appearance of "Fahrenheit" and "Celsius" buttons when one is triggered so that the active scale is highlighted.
   weather.switchToC = function () {
     document.getElementById("temp-far").className = "scale-type inactive";
@@ -41,8 +38,8 @@ app.controller('weatherController', [ '$http', function($http) {
     document.getElementById("temp-cel").className = "scale-type inactive";
     document.getElementById("temp-far").className = "scale-type active";
   };
-
 }]);
+
 
 // Filter to convert given temperatures from Kelvin to Fahrenheit or Celsius; used with ng-show to switch between the two.
 app.filter('convertTemp', [function() {
@@ -62,28 +59,5 @@ app.filter('convertTemp', [function() {
     return convertedTemp + '\u00B0';
   };
 }]);
-
-/*
-app.directive('locator', [function() {
-  return {
-    restrict: 'AEC',
-    template: '<input type="text" ng-model="zipcode" autocomplete="postal-code" pattern="[0-9]{5}" placeholder="Find a new location" required/>'
-  }
-}]);
-*/
-/*
-app.controller('zipcodeController', ['$scope', function($scope) {
-  $scope.zipcode = '5-digit U.S. zipcode';
-}]);
-
-app.controller('urlBuilder', [function() {
-  var url = {
-    base: 'http://api.openweathermap.org/data/2.5/forecast/',
-    type: '',
-    location: '',
-    key: '&APPID=831f9a0e76c47eb878b49f28785cd20b'
-  };
-}]);
-*/
 
 })();
