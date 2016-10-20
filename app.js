fda8692cfc724db34e68e4978759bf6b43ae03d0
egpavelka <elwynpavelka@gmail.com>
2016-10-20 04:15:20 -0400

    added location lookup with autocomplete

--- app.js
+++ app.js
@@ -5,41 +5,114 @@
 // APP
 var app = angular.module('weatherApp', []);
 
-app.controller('weatherController', [ '$http', function($http) {
-  var weather = this;
+app.controller('weatherController', [ '$http', '$scope', function($http, $scope) {
 
-  weather.current = [];
-  weather.hourly = [];
-  weather.daily = [];
+  $scope.current = [];
+  $scope.hourly = [];
+  $scope.daily = [];
     //current
   $http.get('http://api.openweathermap.org/data/2.5/weather?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
   .success(function(response) {
-    weather.current = response;
+    $scope.current = response;
   })
   // hourly
   .then(
   $http.get('http://api.openweathermap.org/data/2.5/forecast?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
   .success(function(response) {
-    weather.hourly = response;
+    $scope.hourly = response;
 
   }))
   // daily
   .then(
   $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=4580543&APPID=831f9a0e76c47eb878b49f28785cd20b')
   .success(function(response) {
-    weather.daily = response;
+    $scope.daily = response;
   }));
 // Change the appearance of "Fahrenheit" and "Celsius" buttons when one is triggered so that the active scale is highlighted.
-  weather.switchToC = function () {
+  $scope.switchToC = function () {
     document.getElementById("temp-far").className = "scale-type inactive";
     document.getElementById("temp-cel").className = "scale-type active";
   };
-  weather.switchToF = function () {
+  $scope.switchToF = function () {
     document.getElementById("temp-cel").className = "scale-type inactive";
     document.getElementById("temp-far").className = "scale-type active";
   };
+
+  ///////////////////////////////////////////////
+  ////////////// TEST ZONE /////////////////////
+
+function setGeolocation() {
+  geolocationService.getGeolocation().then(
+    // If geolocation data returned
+    function(position) {
+      $scope.$apply(function()) {
+        $scope.lat = position.coords.latitude;
+        $scope.lon = position.coords.longitude;
+      }
+    }, function(err) {
+
+    }
+  );
+}
+
+  ///////////////////////////////////////////////
+  ///////////////////////////////////////////////
+
 }]);
 
+///////////////////////////////////////////////
+////////////// TEST ZONE /////////////////////
+app.directive('googleplace', function() {
+    return {
+        require: 'ngModel',
+        scope: {
+            ngModel: '=',
+            details: '=?'
+        },
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {}
+            };
+            scope.searchLocation = new google.maps.places.Autocomplete(element[0], options);
+
+            google.maps.event.addListener(scope.searchLocation, 'place_changed', function() {
+                scope.$apply(function() {
+                    scope.details = scope.searchLocation.getPlace();
+                    model.$setViewValue(element.val());
+                });
+            });
+        }
+    };
+});
+
+//////////// ABOVE OK //////////////////////////
+app.factory('geolocationService', ['$q', '$window', function($q, $window) {
+  function getGeolocation() {
+    var deferred = $q.defer();
+
+    if (!$window.navigator.geolocation) {
+            deferred.reject('Geolocation not supported.');
+        } else {
+            $window.navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    deferred.resolve(position);
+                },
+                function (err) {
+                    deferred.reject(err);
+                });
+        }
+
+        return deferred.promise;
+    }
+
+    return {
+        getCurrentPosition: getCurrentPosition
+    };
+}]);
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
 
 // Filter to convert given temperatures from Kelvin to Fahrenheit or Celsius; used with ng-show to switch between the two.
 app.filter('convertTemp', [function() {
