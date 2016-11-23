2daa16b60bb70b2395204ffb2883145355216cd6
egpavelka <elwynpavelka@gmail.com>
2016-11-23 09:46:02 -0500

    fixed error handling for denied geolocation

--- app.js
+++ app.js
@@ -22,8 +22,10 @@
             geolocationService.detectGeolocation()
                 .then(function(results) {
                     $scope.fetchWeather(results.lat, results.lon);
-                }
-              );
+                }, function(err) {
+                    console.log(err);
+                    $scope.currentStatus = 'geolocationError';
+                });
         };
         //
 
@@ -49,8 +51,8 @@
                     $scope.currentStatus = "weatherReady";
 
                 }, function(err) {
-                  console.log(err);
-                  $scope.currentStatus = 'weatherError';
+                    console.log(err);
+                    $scope.currentStatus = 'weatherError';
                 });
         };
 
@@ -78,7 +80,7 @@
         };
 
         // WATCH WINDOW SIZE: change number of hourly forecasts that appear on each slide and change the width of the weather info container to fit.
-        $scope.buildSlides = function (n) {
+        $scope.buildSlides = function(n) {
             var hourlySlides = [],
                 slide = [];
             for (var i = 0; i < 12; i++) {
@@ -92,57 +94,56 @@
             $scope.hourlySlides = hourlySlides;
         };
 
-        $scope.setSizeScopes = function (w) {
-                      if (w >= 900) {
-                          // desktop
-                          $scope.containerWidth = '870px';
-                          $scope.slideCount = 6;
-                      } else if (w < 900 && w >= 610) {
-                          // tablet
-                          $scope.containerWidth = '590px';
-                          $scope.slideCount = 4;
-                      } else if (w < 610 && w >= 480) {
-                          // large phone
-                          $scope.containerWidth = '450px';
-                          $scope.slideCount = 3;
-                      } else {
-                          // small phone
-                          $scope.containerWidth = '310px';
-                          $scope.slideCount = 2;
-                      }
-                    };
+        $scope.setSizeScopes = function(w) {
+            if (w >= 900) {
+                // desktop
+                $scope.containerWidth = '870px';
+                $scope.slideCount = 6;
+            } else if (w < 900 && w >= 610) {
+                // tablet
+                $scope.containerWidth = '590px';
+                $scope.slideCount = 4;
+            } else if (w < 610 && w >= 480) {
+                // large phone
+                $scope.containerWidth = '450px';
+                $scope.slideCount = 3;
+            } else {
+                // small phone
+                $scope.containerWidth = '310px';
+                $scope.slideCount = 2;
+            }
+        };
         // Initialize on window load
         $scope.view = angular.element($window);
         $scope.setSizeScopes($scope.view.width());
         // Change on window resize
         $scope.view.bind('resize', function() {
-          $scope.setSizeScopes($scope.view.width());
-          $scope.buildSlides($scope.slideCount);
+            $scope.setSizeScopes($scope.view.width());
+            $scope.buildSlides($scope.slideCount);
         });
 
     }]);
 
-// LOCATION, LOCATION, LOCATION
+    // LOCATION, LOCATION, LOCATION
     // Check for geolocation
     app.factory('geolocationService', ['$q', '$window', function($q, $window) {
         return {
-            detectGeolocation: function() {
-                var deferred = $q.defer();
-                var coords = {};
-                // CHECK FOR GEOLOCATION
-                if ($window.navigator && $window.navigator.geolocation) {
-                    $window.navigator.geolocation.getCurrentPosition(function(position) {
-                        coords.lat = position.coords.latitude;
-                        coords.lon = position.coords.longitude;
-                        deferred.resolve(coords);
-                    });
-                } else {
-                  deferred.reject(function(err){
-                    console.log(err);
-                    $scope.currentStatus = 'geolocationError';
+          detectGeolocation: function() {
+              var deferred = $q.defer();
+              var coords = {};
+              // CHECK FOR GEOLOCATION
+              if ($window.navigator && $window.navigator.geolocation) {
+                  $window.navigator.geolocation.getCurrentPosition(function(position) {
+                      coords.lat = position.coords.latitude;
+                      coords.lon = position.coords.longitude;
+                      deferred.resolve(coords);
+                  }, function(err){
+                    deferred.reject(err);
                   });
-                }
-                return deferred.promise;
+              } else {
+                deferred.reject(err);
+              }
+              return deferred.promise;
             }
         };
     }]);
@@ -178,7 +179,7 @@
         };
     }]);
 
-// CHECK THE WEATHER
+    // CHECK THE WEATHER
     // Build URLs for API calls based on location information
     app.factory('urlService', ['$q', function($q) {
         var coords = '';
@@ -230,9 +231,6 @@
                             weatherList[dataKeys[i]] = results[i];
                         }
                         return weatherList;
-                    }, function(err) {
-                        $scope.currentStatus = 'weatherError';
-                        console.log(err);
                     });
             }
         };
@@ -259,46 +257,45 @@
         };
     }]);
 
-// CHANGE VIEW
+    // CHANGE VIEW
     app.directive('weatherReady', function() {
-      return {
-        restrict: 'AE',
-        templateUrl: 'weather.html'
-      };
+        return {
+            restrict: 'AE',
+            templateUrl: 'weather.html'
+        };
     });
 
-app.directive('loadingLocation', function() {
-      return {
-        restrict: 'AE',
-        templateUrl: 'message.html',
-        link: function($scope, element, attrs) {
-          $scope.header = "Locating current position...";
-          $scope.details = "Get the weather for your current location by allowing location services for this page.";
-        }
-      };
-    });
-
-  app.directive('geolocationError', function() {
-      return {
-        restrict: 'AE',
-        templateUrl: 'message.html',
-        link: function($scope, element, attrs) {
-          $scope.header = "Geolocation is not enabled in this browser.";
-          $scope.details = "Try using the search box to look up the weather for another location.  If you'd prefer to use your exact location, please enable your browser's location service and refresh the page, or try clicking on the location detection button above.";
-          $scope.note = "Newer versions of Google Chrome require a secure connection for this feature to work.  You can make sure you're on the secure version of this page by clicking <a href='https://egpavelka.com/weather'>here</a>.";
-        }
-      };
-    });
-
-    app.directive('weatherError', function() {
-          return {
+    app.directive('loadingLocation', function() {
+        return {
             restrict: 'AE',
             templateUrl: 'message.html',
             link: function($scope, element, attrs) {
-              $scope.header = "Something went wrong.";
-              $scope.details = "Weather is not available for this location.  Please search for another location or contact the administrator.";
+                $scope.header = "Locating current position...";
+                $scope.details = "Get the weather for your current location by allowing location services for this page.";
             }
-          };
-        });
+        };
+    });
 
-  })();
+    app.directive('geolocationError', function() {
+        return {
+            restrict: 'AE',
+            templateUrl: 'message.html',
+            link: function($scope, element, attrs) {
+                $scope.header = "Geolocation is not enabled in this browser.";
+                $scope.details = "Try using the search box to look up the weather for another location.  If you'd prefer to use your exact location, please enable your browser's location service and refresh the page, or try clicking on the location detection button above.";
+            }
+        };
+    });
+
+    app.directive('weatherError', function() {
+        return {
+            restrict: 'AE',
+            templateUrl: 'message.html',
+            link: function($scope, element, attrs) {
+                $scope.header = "Something went wrong.";
+                $scope.details = "Weather is not available for this location.  Please search for another location or contact the administrator.";
+            }
+        };
+    });
+
+})();
